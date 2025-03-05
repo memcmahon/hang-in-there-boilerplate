90752c9200eda0133138dfe13732c9cbdd32cc55
memcmahon <mcmahon.meganelizabeth@gmail.com>
2025-03-05 10:54:05 -0700

    wip

--- src/main.js
+++ src/main.js
@@ -10,11 +10,13 @@ var showRandomButton = document.querySelector('button.show-random');
 var showNewPosterButton = document.querySelector('button.show-form');
 var showSavedPostersButton = document.querySelector('button.show-saved');
 var savePosterButton = document.querySelector('button.make-poster');
+var showUnmotivationalPostersButton = document.querySelector('button.show-unmotivational');
 
 //views
 var mainPoster = document.querySelector('section.main-poster');
 var newPosterForm = document.querySelector('section.poster-form');
 var savedPostersView = document.querySelector('section.saved-posters');
+var unmotivationalPostersView = document.querySelector('section.unmotivational-posters');
 
 // form fields
 var imageUrlField = document.querySelector('input[name="poster-image-url"]')
@@ -120,6 +122,128 @@ var quotes = [
   "Each person must live their life as a model for others.",
   "A champion is defined not by their wins but by how they can recover when they fall."
 ];
+var unmotivationalPostersRaw = [
+  {
+    name: "FAILURE",
+    description: "Why bother trying? It's probably not worth it.",
+    price: 68.00,
+    year: 2019,
+    vintage: true,
+    img_url: "./assets/failure.jpg",
+  },
+  {
+    name: "MEDIOCRITY",
+    description: "Dreams are just that—dreams.",
+    price: 127.00,
+    year: 2021,
+    vintage: false,
+    img_url: "./assets/mediocrity.jpg",
+  },
+  {
+    name: "REGRET",
+    description: "Hard work rarely pays off.",
+    price: 89.00,
+    year: 2018,
+    vintage: true,
+    img_url:  "./assets/regret.jpg",
+  },
+  {
+    name: "FUTILITY",
+    description: "You're not good enough.",
+    price: 150.00,
+    year: 2016,
+    vintage: false,
+    img_url:  "./assets/futility.jpg",
+  },
+  {
+    name: "DEFEAT",
+    description: "It's too late to start now.",
+    price: 35.00,
+    year: 2023,
+    vintage: false,
+    img_url:  "./assets/defeat.jpg",
+  },
+  {
+    name: "HOPELESSNESS",
+    description: "Stay in your comfort zone; it's safer.",
+    price: 112.00,
+    year: 2020,
+    vintage: true,
+    img_url: "./assets/hopelessness.jpg",
+  },
+  {
+    name: "LAZINESS",
+    description: "You can't change anything.",
+    price: 25.00,
+    year: 2022,
+    vintage: false,
+    img_url: "./assets/laziness.jpg",
+  },
+  {
+    name: "PROCRASTINATION",
+    description: "Better to avoid failure by not trying at all.",
+    price: 48.00,
+    year: 2017,
+    vintage: true,
+    img_url: "./assets/procrastination.jpg",
+  },
+  {
+    name: "DESPAIR",
+    description: "Let someone else do it; you’ll just mess it up.",
+    price: 73.00,
+    year: 2015,
+    vintage: false,
+    img_url: "./assets/despair.jpg",
+  },
+  {
+    name: "NEGLECT",
+    description: "Happiness is overrated.",
+    price: 160.00,
+    year: 2019,
+    vintage: true,
+    img_url: "./assets/neglect.jpg",
+  },
+  {
+    name: "FEAR",
+    description: "Giving up is always an option.",
+    price: 91.00,
+    year: 2014,
+    vintage: false,
+    img_url: "./assets/fear.jpg",
+  },
+  {
+    name: "APATHY",
+    description: "No one cares about your effort.",
+    price: 110.00,
+    year: 2016,
+    vintage: true,
+    img_url: "./assets/apathy.jpg",
+  },
+  {
+    name: "MISERY",
+    description: "Why take risks when you can stay stagnant?",
+    price: 55.00,
+    year: 2021,
+    vintage: false,
+    img_url: "./assets/misery.jpg",
+  },
+  {
+    name: "BLAME",
+    description: "Expect disappointment and you'll never be disappointed.",
+    price: 39.00,
+    year: 2017,
+    vintage: true,
+    img_url: "./assets/blame.jpg",
+  },
+  {
+    name: "DOUBT",
+    description: "Success is for other people, not you.",
+    price: 140.00,
+    year: 2020,
+    vintage: false,
+    img_url: "./assets/doubt.jpg",
+  }
+];
 var savedPosters = [];
 var currentPoster;
 
@@ -143,6 +267,9 @@ showSavedPostersButton.addEventListener("click", showSaved);
 
 // make poster from form
 savePosterButton.addEventListener("click", storePoster);
+
+// show unmotivational posters
+showUnmotivationalPostersButton.addEventListener("click", showUnmotivational);
 
 // functions and event handlers go here 👇
 // (we've provided two to get you started)!
@@ -170,17 +297,27 @@ function createPoster(imageURL, title, quote) {
 
 function showForm() {
   hide(mainPoster);
+  // hide(unmotivationalPostersView);
   show(newPosterForm)
 }
 
 function showSaved() {
   hide(mainPoster);
+  // hide(unmotivationalPostersView);
   show(savedPostersView);
+}
+
+function showUnmotivational() {
+  hide(mainPoster);
+  // hide(newPosterForm);
+  // hide(savedPostersView);
+  show(unmotivationalPostersView);
 }
 
 function showMain() {
   hide(savedPostersView);
-  hide(newPosterForm)
+  hide(newPosterForm);
+  hide(unmotivationalPostersView);
   show(mainPoster);
 }
 
